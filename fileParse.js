f21d4ba7a6eda189eff4c54c780f1b066df62315
Peoples Champion <95397105+ThePeoplesChamp@users.noreply.github.com>
2022-08-18 10:40:09 +0930

    Update fileParse.js

--- fileParse.js
+++ fileParse.js
@@ -3,30 +3,25 @@ const readline = require('readline')
 const { uniqueIPs, mostThreeCommon } = require('./logic')
 
 
-const fileParse = () => {
-    let logFile = process.argv[2]
-    let IPs = []
-    let URLs = []
+let logFile = process.argv[2]
+let IPs = []
+let URLs = []
 
-    const rdln = readline.createInterface({
-        input: fs.createReadStream(`${logFile}.log`),
-        output: process.stdout,
-        terminal: false
-    })
+const rdln = readline.createInterface({
+    input: fs.createReadStream(`${logFile}.log`),
+    output: process.stdout,
+    terminal: false
+})
 
-    rdln.on('line', ((line) => {
-        const ip = line.match(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/g)
-        IPs.push(ip[0])
-        const url = line.substring(
-            line.indexOf("GET") + 4,
-            line.lastIndexOf(" HTTP"));
-        URLs.push(url)
-    }))
+rdln.on('line', ((line) => {
+    const ip = line.match(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/g)
+    IPs.push(ip[0])
+    const url = line.substring(
+        line.indexOf("GET") + 4,
+        line.lastIndexOf(" HTTP"));
+    URLs.push(url)
+}))
 
-    rdln.on('close', (() => {
-        console.log(`Within this log file there are ${uniqueIPs(IPs)} unique IP adresses, the top three most active being ${mostThreeCommon(IPs)[0]}, ${mostThreeCommon(IPs)[1]} and ${mostThreeCommon(IPs)[2]}. the top three most visited URLs were ${mostThreeCommon(URLs)[0]}, ${mostThreeCommon(URLs)[1]} and ${mostThreeCommon(URLs)[2]}`)
-    }))
-}
-fileParse();
-
-module.exports = { fileParse }
+rdln.on('close', (() => {
+    console.log(`Within this log file there are ${uniqueIPs(IPs)} unique IP adresses, the top three most active being ${mostThreeCommon(IPs)[0]}, ${mostThreeCommon(IPs)[1]} and ${mostThreeCommon(IPs)[2]}. the top three most visited URLs were ${mostThreeCommon(URLs)[0]}, ${mostThreeCommon(URLs)[1]} and ${mostThreeCommon(URLs)[2]}`)
+}))
